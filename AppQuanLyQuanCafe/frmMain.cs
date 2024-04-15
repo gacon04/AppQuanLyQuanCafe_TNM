b3594b3946171cc474b834e3304cf04fca59c6fd
ngocminh04 <crushzone610@gmail.com>
2024-04-16 05:49:57 +0700

    -update

--- AppQuanLyQuanCafe/frmMain.cs
+++ AppQuanLyQuanCafe/frmMain.cs
@@ -18,7 +18,7 @@ namespace AppQuanLyQuanCafe
             InitializeComponent();
             DateTime currentTime = DateTime.Now;
             timer = new Timer();
-            timer.Interval = 900;
+            timer.Interval = 999;
 
             // Gắn sự kiện Tick của Timer với phương thức xử lý sự kiện Timer_Tick
             timer.Tick += Timer_Tick;
